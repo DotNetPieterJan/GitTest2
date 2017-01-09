3e877830706a87dae9dac072df5cb9efe6bb52c9
Pieter-Jan Servaes <pieterjan105@gmail.com>
2017-01-09 09:48:12 +0100

    adding initial test code

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -10,6 +10,15 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("Hello!");
+            }
+            Console.ReadKey();
         }
     }
 }
